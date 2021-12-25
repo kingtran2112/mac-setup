3cc8b02b61063b4ce6252cc6e1da22bb39d74939
King Tran <kingtran.2112@gmail.com>
2021-12-25 19:27:21 +0700

    Configuration for zpreztorc

--- setup.sh
+++ setup.sh
@@ -24,7 +24,7 @@ fi
 
 echo "Config git"
 git config --global user.name "King Tran"
-git config --global user.email kingtran.2112@gmail.com
+git config --global user.email "kingtran.2112@gmail.com"
 
 # Install prezto
 echo "Start install prezto"
@@ -40,11 +40,20 @@ then
 
   echo "Set zsh as default shell"
   chsh -s /bin/zsh
-  
+
+  echo "Update zpreztorc file"
+  # By default, cp may run with -i flag (check by {@link alias | grep cp})
+  # so we use backward slash to avoid it
+  # We can also use sed command to change the configuration or add the module
+  # we want but we think directly copy it like this is more straightforward and easier
+  \cp -v zpreztorc ~/.zpreztorc
+
   echo "Finish install prezto"
 else
   echo "Prezto is already installed, skiped!!!"
 fi
+
+applications=(visual-studio-code )
 
 #Install visual studio code
 echo "Start install vscode"
