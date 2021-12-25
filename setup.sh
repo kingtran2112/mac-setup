4dee7f5da1d0d7f47188dcd9592a790167bad1bf
Kawaii <kawaii@Kawaii-MacBook-Pro.local>
2021-12-25 17:48:11 +0700

    Init basic tool

---
+++ setup.sh
@@ -0,0 +1,56 @@
+#Install homebrew
+echo "Start install homebrew"
+if ! isApplicationExist brew
+then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  echo "Finish install homebrew"
+else
+  echo "Brew is already installed, skiped!!!"
+fi
+
+#Install iterm
+echo "Start install iterm2"
+brew install --cask iterm2
+echo "Finish install iterm2"
+
+#Install git
+echo "Start install"
+if ! isApplicationExist git
+then
+  brew install git
+else
+  echo "Git is already installed, skiped!!!"
+fi
+
+echo "Config git"
+git config --global user.name "King Tran"
+git config --global user.email kingtran.2112@gmail.com
+
+# Install prezto
+echo "Start install prezto"
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+
+echo "Set up prezto"
+if ! isApplicationExist prompt
+then
+  setopt EXTENDED_GLOB
+  for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+    ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+  done
+
+  echo "Set zsh as default shell"
+  chsh -s /bin/zsh
+  
+  echo "Finish install prezto"
+else
+  echo "Prezto is already installed, skiped!!!"
+fi
+
+#Install visual studio code
+echo "Start install vscode"
+brew install --cask visual-studio-code
+echo "Finish install vscode"
+
+isApplicationExist() {
+  command -v $1 &> /dev/null
+}
