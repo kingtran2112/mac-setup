785e4fc9742bf05ea415d9274555a816318e07ea
King Tran <kingtran.2112@gmail.com>
2023-01-28 17:39:52 +0700

    Typo

--- setup.sh
+++ setup.sh
@@ -74,10 +74,10 @@ declare -A applications
 applications=(
   ["Visual Studio Code"]="visual-studio-code"
   ["Google Chrome"]="google-chrome"
-  ["Warp"]="warp",
-  ["Slack"]="slack",
-  ["Discord"]="discord",
-  ["Hammerspoon"]="hammerspoon",
+  ["Warp"]="warp"
+  ["Slack"]="slack"
+  ["Discord"]="discord"
+  ["Hammerspoon"]="hammerspoon"
   ["Rectangle"]="rectangle"
 )
 
